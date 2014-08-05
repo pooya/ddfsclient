75c42e59e16255fbaaf74f6d405d0d6fc7b592e6
Shayan Pooya <shayan@chango.com>
2014-08-05 09:47:28 -0400

    Test multiple replicas

--- Main.java
+++ Main.java
@@ -1,16 +1,21 @@
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Main {
 	public static void main(String[] args) {
 		Ddfs ddfs = new Ddfs("localhost", "8989");
-		Tag tag = ddfs.getTag("h");
-		String thisBlob = tag.getUrls().get(0).get(0);
+		Tag tag = ddfs.getTag("h2");
 
-		InputStream is = ddfs.getBlob(thisBlob);
-		Iterator<Object> objs = new DiscoRDD(is);
-		while (objs.hasNext()) {
-			System.out.println(objs.next());
+		ArrayList<ArrayList<String>> urls = tag.getUrls();
+		for (ArrayList<String> replica : urls) {
+			String thisBlob = replica.get(0);
+			InputStream is = ddfs.getBlob(thisBlob);
+			Iterator<Object> objs = new DiscoRDD(is);
+			while (objs.hasNext()) {
+				System.out.println(objs.next());
+			}
 		}
+
 	}
 }
