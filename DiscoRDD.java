0df51e6dfcd6c0f23732a6e2a4d069bfade663b0
Shayan Pooya <shayan@chango.com>
2014-08-05 11:18:22 -0400

    clean up and remove unnecessary code

--- DiscoRDD.java
+++ DiscoRDD.java
@@ -28,28 +28,20 @@ public class DiscoRDD implements Iterator<Object> {
 		try {
 			inputStream.read(compressedHunk);
 			Inflater decompressor = new Inflater();
-//			System.out.println("trying to read " + hunkSize);
 			decompressor.setInput(compressedHunk);
 			decompressor.inflate(uncompressedHunk);
-//			System.out.println(uncompressedHunk);
 			InputStream hunkInputStream = new ByteArrayInputStream(
 					uncompressedHunk);
 			while (true) {
 				try {
 					Object obj = unpickler.load(hunkInputStream);
 					objects.add(obj);
-//					System.out.println(obj);
 				} catch (PickleException e) {
 					break;
 				}
 			}
 		} catch (IOException e1) {
-//			System.out.println("before exception: " + objects.size());
-//			System.err.println("IOException");
-//			e1.printStackTrace();
 		} catch (DataFormatException e1) {
-//			System.err.println("DataFormatException");
-//			e1.printStackTrace();
 		}
 		hunkSize = -1;
 	}
@@ -69,10 +61,8 @@ public class DiscoRDD implements Iterator<Object> {
 		}
 		readNextHunk();
 		if (hunkSize == -1) {
-//			System.out.println("returning false: " + objects.size());
 			return false;
 		} else {
-//			System.out.println("hunkSize is: " + hunkSize);
 			readObjectsFromHunk();
 			return !objects.isEmpty();
 		}
@@ -85,7 +75,6 @@ public class DiscoRDD implements Iterator<Object> {
 
 	@Override
 	public void remove() {
-		throw new RuntimeException("should not call remove");
-//		objects.remove(0);
+		throw new RuntimeException("remove not supported yet.");
 	}
 }
--- UnChunker.java
+++ UnChunker.java
@@ -1,12 +1,5 @@
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
-import java.util.zip.DataFormatException;
-import java.util.zip.Inflater;
-
-import net.razorvine.pickle.PickleException;
-import net.razorvine.pickle.Unpickler;
 
 public class UnChunker {
 	private static final int HEADER_SIZE = 14;
@@ -16,17 +9,16 @@ public class UnChunker {
 		try {
 			long nRead = is.read(chunkHeader);
 			if (nRead != HEADER_SIZE) {
-//				throw new IOException("bad header size: " + nRead);
 				return -1;
 			}
 			int version = 0xff & chunkHeader[0];
 			long isCompressed = (0xff & chunkHeader[1]);
-			long checkSum = 0;
-
-			for (int i = 2, offset = 0; i < 6; i++) {
-				checkSum += (0xff & chunkHeader[i]) << offset;
-				offset += 8;
-			}
+			/*
+			 * TODO add checksum verification: long checkSum = 0;
+			 * 
+			 * for (int i = 2, offset = 0; i < 6; i++) { checkSum += (0xff &
+			 * chunkHeader[i]) << offset; offset += 8; }
+			 */
 
 			int hunkSize = 0;
 
@@ -34,46 +26,16 @@ public class UnChunker {
 				hunkSize += (0xff & chunkHeader[i]) << offset;
 				offset += 8;
 			}
-//			System.out.println(version + " : " + isCompressed + " : "
-//					+ checkSum + " : " + hunkSize);
+			if (version != 129) {
+				throw new RuntimeException("not implemented yet");
+			}
+			if (isCompressed != 1) {
+				throw new RuntimeException("not implemented yet");
+			}
 			return hunkSize;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return -1;
-	}
-
-	public static Iterator<String> getElementsOfBlob(InputStream is) {
-		Unpickler un = new Unpickler();
-		try {
-			int hunkSize = UnChunker.unchunk(is);
-
-			while (hunkSize != -1) {
-				byte[] compressedHunk = new byte[hunkSize];
-				byte[] uncompressedHunk = new byte[hunkSize];
-
-				is.read(compressedHunk);
-				Inflater decompressor = new Inflater();
-				decompressor.setInput(compressedHunk);
-				decompressor.inflate(uncompressedHunk);
-				System.out.println(uncompressedHunk);
-				InputStream hunkInputStream = new ByteArrayInputStream(
-						uncompressedHunk);
-				while (true) {
-					try {
-						Object obj = un.load(hunkInputStream);
-						System.out.println(obj);
-					} catch (PickleException e) {
-						break;
-					}
-				}
-				hunkSize = UnChunker.unchunk(is);
-			}
-		} catch (IOException e) {
-			return null;
-		} catch (DataFormatException e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 }
